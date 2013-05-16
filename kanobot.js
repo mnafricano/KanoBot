900bfff4bfe3496223e875a3f859cd918e00accd
Marcello Africano <mnafricano@gmail.com>
2013-05-16 13:12:56 -0700

    Updated kanobot.js

--- kanobot.js
+++ kanobot.js
@@ -9,10 +9,32 @@ var AUTH   = 'xxxxxxxxxxxxxxxxxxxxxxxx';
 var USERID = 'xxxxxxxxxxxxxxxxxxxxxxxx';
 var ROOMID = 'xxxxxxxxxxxxxxxxxxxxxxxx';
 
-var bot = new Bot(AUTH, USERID)
+var bot = new Bot(AUTH, USERID);
+
+/* ===== Debugging ===== */
+var debug = false;
+
+
+/* ======= Public Chat [speak] Event ======= */
+bot.on('speak', function(data) {
+  // [Debugging] Log to console
+  if (debug = true) {
+    console.log('Someone said something.');
+  }
+  else {
+    
+  }
+}
+
+/* ===== Private Message [pmmed] Event ===== */
 
 // When the bot recieves a message, tell the console to say that he has.
 bot.on('pmmed', function(data) {
-  // Log to console.
-  console.log('The bot has recieved a message.');
+  // [Debugging] Log to console.
+  if (debug = true) {
+    console.log('The bot has recieved a message.');
+  }
+  else {
+    
+  }
 });
